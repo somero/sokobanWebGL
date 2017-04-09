eaf44237088b45ae43bd5939bff3aa3b7bb9c31c
Omer Ofir <omer@skycure.com>
2017-04-09 23:05:24 +0300

    changed to dynamic port2

--- sokobanCore.js
+++ sokobanCore.js
@@ -29,7 +29,4 @@ http.createServer( function (request, response) {
       // Send the response body 
       response.end();
    });   
-}).listen(process.env.PORT);
-
-// Console will print the message
-console.log('Server running at http://127.0.0.1:8081/');
+}).listen(process.env.PORT || 8000);
